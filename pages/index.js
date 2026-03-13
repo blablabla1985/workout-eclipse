f58a34ac545e3c7a38377eb9418a20bf491e70be
blablabla1985 <140961682+blablabla1985@users.noreply.github.com>
2026-03-13 23:47:11 +0100

    Add pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+export default function Home() { return null }
+export async function getServerSideProps({ res }) {
+  res.writeHead(302, { Location: '/app.html' })
+  res.end()
+  return { props: {} }
+}
